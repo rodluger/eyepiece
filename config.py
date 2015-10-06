a39d6bae15e423fdaa937595cb51142fa557d9e7
Rodrigo Luger <rodluger@gmail.com>
2015-10-06 16:28:17 -0700

    several updates and bugfixes

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+config.py
+---------
+
+Config file for :py:mod:``eyepiece.py``. Set default values here.
+
+'''
+
+dir =                 '/Users/rod/src/templar/output'
+ttvpath =             '/Users/rod/src/templar/ttvs'
--- eyepiece.py
+++ eyepiece.py
@@ -8,10 +8,14 @@ Download and visually inspect, split, and correct Kepler lightcurves.
 
 '''
 
+import config
 import matplotlib.pyplot as pl
 import numpy as np
 import kplr
 import os
+
+KEPLONGEXP =              (1765.5/86400.)
+KEPSHRTEXP =              (58.89/86400.)
 
 def EmptyData(quarters):
   '''
@@ -37,10 +41,52 @@ def GetKoi(koi):
     raise ValueError("No KOI found with the number: '{0}'".format(koi))
   return kois[0]
 
+def GetTransitTimes(koi, tstart, tend, pad = 4.0, ttvs = False, long_cadence = True):
+  '''
+  
+  '''
+
+  planet = GetKoi(koi)
+  per = planet.koi_period
+  t0 = planet.koi_time0bk
+  tdur = pad * planet.koi_duration/24.
+  
+  if ttvs:
+    # Get transit times (courtesy Ethan Kruse)
+    try:
+      with open(os.path.join(config.ttvpath, "KOI%.2f.ttv" % koi), "r") as f:
+        lines = [l for l in f.readlines() if ('#' not in l) and (len(l) > 1)]
+        tcads = np.array([int(l.split('\t')[1]) for l in lines], dtype = 'int32')
+    except IOError:
+      raise Exception("Unable to locate TTV file for the target.")
+  
+    # Ensure t0 is in fact the first transit
+    t0 -= per * divmod(t0 - tstart, per)[0]
+  
+    # Calculate transit times from cadences
+    if long_cadence:
+      tN = t0 + (tcads - tcads[0]) * KEPLONGEXP
+    else:
+      tN = t0 + (tcads - tcads[0]) * KEPSHRTEXP
+
+    # Ensure our transit times go all the way to the end, plus a bit
+    # (the extra per/2 is VERY generous; we're likely fitting for one
+    # or two more transits than we have data for.)
+    while tN[-1] < tend + per/2.:
+      tN = np.append(tN, tN[-1] + per)
+  
+  else:
+    n, r = divmod(tstart - t0, per)
+    if r < tdur/2.: t0 = t0 + n*per
+    else: t0 = t0 + (n + 1)*per
+    tN = np.arange(t0, tend + per, per)
+
+  return tN, per, tdur
+
 def GetTPFData(koi, long_cadence = True, clobber = False, 
-               bad_bits = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17],
-               aperture = 'optimal', quarters = range(18), dir = '',
-               quiet = False):
+               bad_bits = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17], pad = 4.0,
+               aperture = 'optimal', quarters = range(18), dir = config.dir,
+               ttvs = False, quiet = False):
   '''
   
   '''
@@ -48,17 +94,23 @@ def GetTPFData(koi, long_cadence = True, clobber = False,
   if not clobber:
     try:
       data = np.load(os.path.join(dir, str(koi), 'data_raw.npz'))['data']
+      foo = np.load(os.path.join(dir, str(koi), 'transits.npz'))
+      tN = foo['tN']
+      per = foo['per']
+      tdur = foo['tdur']
       if not quiet: print("Loading saved TPF data...")
-      return data
+      return data, tN, per, tdur
     except:
       pass
   
   if not quiet: print("Downloading TPF data...")
-  star = GetKoi(koi)
+  planet = GetKoi(koi)  
   data = EmptyData(quarters)
-  tpf = star.get_target_pixel_files(short_cadence = not long_cadence)
+  tpf = planet.get_target_pixel_files(short_cadence = not long_cadence)
   if len(tpf) == 0:
     raise Exception("No pixel files for selected object!")
+  tstart = np.inf
+  tend = -np.inf
   for fnum in range(len(tpf)):
     with tpf[fnum].open(clobber = clobber) as f:  
       ap = f[2].data
@@ -98,12 +150,19 @@ def GetTPFData(koi, long_cadence = True, clobber = False,
     perr = np.delete(perr, bad_inds, 0)
     data[quarter].update({'time': time, 'fsum': fsum, 'ferr': ferr, 'fpix': fpix, 
                           'perr': perr, 'cad': cad, 'crowding': crowding})
+    if time[0] < tstart: tstart = time[0]
+    if time[-1] > tend: tend = time[-1]
   
   if not os.path.exists(os.path.join(dir, str(koi))):
     os.makedirs(os.path.join(dir, str(koi)))
   np.savez(os.path.join(dir, str(koi), 'data_raw.npz'), data = data)
   
-  return data
+  # Now get the transit info
+  tN, per, tdur = GetTransitTimes(koi, tstart, tend, pad = pad, ttvs = ttvs, 
+                                  long_cadence = long_cadence)
+  np.savez(os.path.join(dir, str(koi), 'transits.npz'), tN = tN, per = per, tdur = tdur)
+    
+  return data, tN, per, tdur
 
 def AddVLine(ax, t):
   '''
@@ -115,18 +174,18 @@ class Interactor(object):
   '''
   
   '''
-  def __init__(self, fig, ax, x, y, p, split_cads = [], cad_tol = 10, min_sz = 300,  
-               style = '.', title = ''):
+  def __init__(self, fig, ax, t, x, y, p, tN, per, tdur, split_cads = [], 
+               cad_tol = 10, min_sz = 300, title = ''):
     self.fig = fig
     self.ax = ax
+    self.t = np.array(t)
     self.x = np.array(x)
     self.y = np.array(y)
     self.p = np.array(p)
-    self.style = style
-    self.plots = [self.ax.plot(self.x, self.y, self.style)]
-    self.odots = []
+    self.tN = np.array(tN)
+    self.per = per
+    self.tdur = tdur
     self.fig.canvas.mpl_connect('key_press_event', self.on_key_press) 
-    self.lines = []
     self.state = 'fsum'
     self._jumps = []
     self._outliers = np.array([], dtype = int)
@@ -168,58 +227,68 @@ class Interactor(object):
     self._jumps.extend(list(cut))
     self._jumps = np.array(list(set(self._jumps) - set([0, len(self.x) - 1])), dtype = int)
     
+    # Figure out the transit indices
+    self.tcads = []
+    for ti in self.tN:
+      i = np.where(np.abs(self.t - ti) <= self.tdur)
+      self.tcads.extend(self.x[i])
+    self.tcads = np.array(self.tcads, dtype = int)
+    
     self.redraw()
   
   def redraw(self):
   
     # Redraw the figure
-    for plot in self.plots:
-      plot.pop(0).remove()
-    for odot in self.odots:
-      odot.pop(0).remove()
-    while len(self.lines): 
-      self.lines[0].remove()
-      del self.lines[0]
     pl.cla()
     self.ax.set_color_cycle(None)
       
-    self.plots = []
-    self.odots = []
-    self.lines = []
     ssi = [0] + sorted(self._jumps) + [-1]
 
     # Plot the SAP flux
     if self.state == 'fsum':
     
       for a, b in zip(ssi[:-1], ssi[1:]):
-        plot = self.ax.plot(self.x[a:b], self.y[a:b], self.style)
-        self.plots.append(plot)
+        
+        # Plot the data
+        plot, = self.ax.plot(self.x[a:b], self.y[a:b], '.')
+        
+        # Plot the transits
+        x = []; y = []
+        for tc in self.tcads:
+          if self.x[a] <= tc and tc <= self.x[b]:
+            x.append(tc)
+            y.append(self.y[np.argmax(self.x == tc)])
+        self.ax.plot(x, y, '.', markersize = 15, color = plot.get_color(), zorder = -1, alpha = 0.25)
        
       for s in self._jumps:
-        self.lines.append(AddVLine(self.ax, (self.x[s] + self.x[s - 1]) / 2.))
+        line = AddVLine(self.ax, (self.x[s] + self.x[s - 1]) / 2.)
 
       for o in self._outliers:
         odot = self.ax.plot(self.x[o], self.y[o], 'ro')
-        self.odots.append(odot)
     
     # Plot the pixel fluxes
     elif self.state == 'fpix':
     
       for a, b in zip(ssi[:-1], ssi[1:]):
         for p in np.transpose(self.p):
-          plot = self.ax.plot(self.x[a:b], p[a:b], self.style)
-          self.plots.append(plot)
+          plot, = self.ax.plot(self.x[a:b], p[a:b], '.')
+          
+          # Plot the transits
+          x = []; y = []
+          for tc in self.tcads:
+            if self.x[a] <= tc and tc <= self.x[b]:
+              x.append(tc)
+              y.append(p[np.argmax(self.x == tc)])
+          self.ax.plot(x, y, '.', markersize = 15, color = plot.get_color(), zorder = -1, alpha = 0.25)
        
       for s in self._jumps:
-        self.lines.append(AddVLine(self.ax, (self.x[s] + self.x[s - 1]) / 2.))
+        line = AddVLine(self.ax, (self.x[s] + self.x[s - 1]) / 2.)
 
       for o in self._outliers:
         for p in np.transpose(self.p):
           odot = self.ax.plot(self.x[o], p[o], 'ro')
-        self.odots.append(odot)
-        
+            
     self.ax.margins(0.01,0.1)
-    
     self.ax.set_title(self.title, fontsize = 24)
     self.ax.set_xlabel('Cadence Number', fontsize = 22)
     self.ax.set_ylabel('Flux', fontsize = 22)
@@ -319,9 +388,9 @@ class Interactor(object):
     return np.array(sorted(self._jumps), dtype = int)
   
 def View(koi = 17.01, long_cadence = True, clobber = False,
-         bad_bits = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17],
+         bad_bits = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17], pad = 4.0,
          aperture = 'optimal', quarters = range(18), min_sz = 300,
-         dt_tol = 0.5, split_cads = [4472, 6717], dir = '',
+         dt_tol = 0.5, split_cads = [4472, 6717], dir = config.dir, ttvs = False,
          quiet = False):
   '''
   
@@ -329,9 +398,9 @@ def View(koi = 17.01, long_cadence = True, clobber = False,
 
   # Grab the data
   if not quiet: print("Retrieving target data...")
-  data = GetTPFData(koi, long_cadence = long_cadence, clobber = clobber, dir = dir,
-                    bad_bits = bad_bits, aperture = aperture, quarters = quarters,
-                    quiet = quiet)
+  data, tN, per, tdur = GetTPFData(koi, long_cadence = long_cadence, clobber = clobber, dir = dir,
+                        bad_bits = bad_bits, aperture = aperture, quarters = quarters,
+                        quiet = quiet, pad = pad, ttvs = ttvs)
   data_new = EmptyData(quarters)
   
   # Loop over all quarters
@@ -352,7 +421,8 @@ def View(koi = 17.01, long_cadence = True, clobber = False,
     
     # Set up the plot
     fig, ax = pl.subplots(1, 1, figsize = (16, 6))
-    itr = Interactor(fig, ax, data[q]['cad'], data[q]['fsum'], data[q]['fpix'], 
+    itr = Interactor(fig, ax, data[q]['time'], data[q]['cad'], data[q]['fsum'], 
+                     data[q]['fpix'], tN, per, tdur,
                      min_sz = min_sz, split_cads = split_cads, cad_tol = cad_tol,
                      title = 'KOI %.2f: Quarter %02d' % (koi, q))
     
@@ -396,4 +466,4 @@ def View(koi = 17.01, long_cadence = True, clobber = False,
   return
 
 if __name__ == '__main__':
-  View()
+  View(koi = 254.01)
