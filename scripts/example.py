df3d6d53064135e66d67ef5ea8a4a98f27e1e8d0
Rodrigo Luger <rodluger@gmail.com>
2015-10-12 15:16:12 -0700

    bugfixes

--- scripts/example.py
+++ scripts/example.py
@@ -7,5 +7,6 @@ example.py
 '''
 
 import eyepiece
-eyepiece.Inspect(koi = 254.01)
+
+# eyepiece.Inspect(koi = 254.01)
 eyepiece.pld.PLD(koi = 254.01, quarters = list(range(1,18)), niter = 2, maxfun = 50, debug = False)
