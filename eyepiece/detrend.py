beff0913e71b083e240d52646c235784539b4b0b
Rodrigo Luger <rodluger@gmail.com>
2015-10-17 18:50:12 -0700

    bugfix

--- eyepiece/detrend.py
+++ eyepiece/detrend.py
@@ -362,6 +362,6 @@ def PlotDetrended(koi = 17.01, quarters = list(range(18)), kernel = 1. * george.
       axis.axvline(lt[q], color='k', ls = '--')
     ltq = lt[q]
    
-  ax[1].set_title('PLD-Decorrelated Flux', fontsize = 24)  
-  ax[2].set_title('PLD+GP-Decorrelated Flux', fontsize = 24)   
+  ax[1].set_title('PLD-Decorrelated Flux', fontsize = 18)  
+  ax[2].set_title('PLD+GP-Decorrelated Flux', fontsize = 18)   
   fig.savefig(os.path.join(datadir, str(koi), 'pld', 'decorr.png'), bbox_inches = 'tight')
