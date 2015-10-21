5ddce6056fd5748258ca6be1191ac96a805e247f
Rodrigo Luger <rodluger@gmail.com>
2015-10-21 15:37:03 -0700

    analytical gradient

---
+++ eyepiece/lnlike.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+lnlike.py
+---------
+
+'''
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+import numpy as np
+import george
+
+def LnLike(x, time, fpix, perr, fsum = None, tmod = None, lndet = True,
+           kernel = 1. * george.kernels.Matern32Kernel(1.)):
+  '''
+  Returns the *Ln(Likelihood)* of the model given an array of parameters ``x``,
+  as well as its gradient (computed analytically) with respect to ``x``.
+  
+  :param x: The array of model parameters, starting with \
+            the GP kernel parameters and followed by the \
+            PLD pixel coefficients.
+  :type x: :class:``numpy.ndarray``
+  
+  :param time: The array of times at which the data was taken
+  :type time: :class:``numpy.ndarray``
+  
+  :param fpix: An array of shape ``(N, NP)`` of the individual pixel fluxes, where \
+               ``N`` is the number of data points and ``NP`` is the number of pixels \
+               in the aperture
+  :type fpix: :class:``numpy.ndarray``
+  
+  :param perr: An array of shape ``(N, NP)`` of the errors on the pixel fluxes
+  :type perr: :class:``numpy.ndarray``
+  
+  :param fsum: An array of length ``N`` corresponding to the total flux at each time \
+               point. If not provided, this is computed by summing over the second \
+               axis of ``fpix``.
+  :type fsum: :class:``numpy.ndarray``, optional
+  
+  :param tmod: An array of length ``N`` corresponding to the transit model. If not \
+               provided, defaults to ``np.ones(N)`` (i.e., no transit)
+  :type tmod: :class:``numpy.ndarray``, optional
+  
+  :param lndet: Whether or not to compute the contribution of the *ln|K|* term to \
+                the gradient. This contribution is usually small and may be \
+                neglected in some cases to speed up the computation. Default is \
+                ``True``
+  :type fpix: :type:``bool``, optional
+  
+  :param kernel: The kernel to use in the GP computation. Default is a *Matern 3/2* \
+                 kernel
+  :type fpix: :class:``george.kernels.Kernel``
+  
+  '''
+  
+  # Calculate fsum if not provided
+  if fsum is None:
+    fsum = np.array(fpix, axis = 1)
+  
+  # PLD coefficients
+  c = x[len(kernel.pars):]
+  
+  # Kernel params
+  kernel.pars = x[:len(kernel.pars)]
+  
+  # Number of data points
+  K = len(time)
+  
+  # Inverse of the transit model
+  if tmod is None:
+    D = np.ones_like(time)
+  else:
+    D = 1. / tmod
+  
+  # The pixel model
+  pmod = np.sum(fpix * np.outer(1. / fsum, c), axis = 1)
+  
+  # The PLD-detrended data
+  y = fsum - pmod
+
+  # Errors on detrended data (y)
+  X = D + pmod / fsum
+  B = X.reshape(K, 1) * perr - c * perr / fsum.reshape(K, 1)
+  yerr = np.sum(B ** 2, axis = 1) ** 0.5
+
+  # Compute the likelihood
+  gp = george.GP(kernel)
+  gp.compute(time, yerr)
+  ll = gp.lnlikelihood(y)
+
+  # Compute the gradient of the likelihood with respect to the PLD coefficients
+  # First, the gradient assuming the covariance is independent of the coeffs
+  A = gp.solver.apply_inverse(y)
+  R = fpix / fsum.reshape(K, 1)
+  grad_pld = np.dot(R.T, A)
+  
+  # Pre-compute some stuff
+  H = np.array([perr[k] * (X[k] - (c / fsum[k])) for k in range(K)])
+  J = np.array([perr[k] / fsum[k] for k in range(K)])
+  
+  # Are we computing the very small contribution from the ln-det term?
+  # If so, pre-compute the diagonal of the inverse covariance matrix
+  if lndet:
+    KID = np.diag(gp.solver.apply_inverse(np.eye(K)))
+  
+  # Now add the covariance term  
+  for m, _ in enumerate(grad_pld):
+
+    # The Kronecker delta
+    KD = np.zeros_like(grad_pld); KD[m] = 1
+    
+    # Derivative of sigma^2 with respect to the mth PLD coefficient
+    dS2dC = 2 * np.diag(np.sum(H * (J * R[:,m].reshape(K,1) - np.outer(J[:,m], KD)), axis = 1))
+    
+    # The covariance term
+    grad_pld[m] += 0.5 * np.dot(A.T, np.dot(dS2dC, A))
+    
+    if lndet:
+      # The small ln(det) term
+      grad_pld[m] -= np.dot(KID, np.diag(dS2dC))
+  
+  # Append the PLD gradient to the GP gradient
+  # Note that george returns dLnLike/dLnX, so we divide by X to get dLnLike/dX.
+  grad_ll = np.append(gp.grad_lnlikelihood(y) / kernel.pars, grad_pld)
+  
+  return (ll, grad_ll)
--- scripts/grad.py
+++ scripts/grad.py
@@ -4,16 +4,30 @@
 grad.py
 -------
 
-A test of our analytical log-likelihood gradient calculation.
+A test of our analytical log-likelihood gradient calculation. It's
+faster than the numerical gradient by a factor of ~2x in the example case:
+
+>>> Numerical time:  1.5756903096000315
+>>> Analytical time: 0.7447342678002315
+
+For a smaller chunk (Quarter 4, chunk 1), it's even faster:
+
+>>> Numerical time:  0.15853579879985774
+>>> Analytical time: 0.046326947699708396
+
+Note that for some reason ``SlowAnalyticalGradient`` give numbers that are in
+better agreement with ``NumericalGradient``, even though the math is (or should be!)
+identical. The disagreement is at the 1e-6 level, which is below our precision
+anyways, but I should look into this eventually.
 
 '''
 
 from eyepiece import GetData
-import numpy as np; np.random.seed(12345)
+import numpy as np; np.random.seed(123)
 import george
 import timeit
 
-def FastAnalyticalGradient(coeffs, time, fsum, fpix, perr, kernel = 1. * george.kernels.Matern32Kernel(1.), tmod = None, lndet = True):
+def AnalyticalGradient(coeffs, time, fsum, fpix, perr, kernel = 1. * george.kernels.Matern32Kernel(1.), tmod = None, lndet = True):
   
   # PLD coefficients
   c = coeffs[len(kernel.pars):]
@@ -49,12 +63,17 @@ def FastAnalyticalGradient(coeffs, time, fsum, fpix, perr, kernel = 1. * george.
   # Compute the gradient of the likelihood with respect to the PLD coefficients
   # First, the gradient assuming the covariance is independent of the coeffs
   A = gp.solver.apply_inverse(y)
-  R = fpix.T / fsum
-  grad_pld = np.dot(R, A)
+  R = fpix / fsum.reshape(K, 1)
+  grad_pld = np.dot(R.T, A)
   
   # Pre-compute some stuff
   H = np.array([perr[k] * (X[k] - (c / fsum[k])) for k in range(K)])
   J = np.array([perr[k] / fsum[k] for k in range(K)])
+  
+  # Are we computing the very small contribution from the ln-det term?
+  # If so, pre-compute the diagonal of the inverse covariance matrix
+  if lndet:
+    KID = np.diag(gp.solver.apply_inverse(np.eye(K)))
   
   # Now add the covariance term  
   for m, _ in enumerate(grad_pld):
@@ -63,20 +82,20 @@ def FastAnalyticalGradient(coeffs, time, fsum, fpix, perr, kernel = 1. * george.
     KD = np.zeros_like(grad_pld); KD[m] = 1
     
     # Derivative of sigma^2 with respect to the mth PLD coefficient
-    dS2dC = 2 * np.diag([np.sum(H[k] * (J[k] * R[m,k] - J[k,m] * KD )) for k in range(K)])
+    dS2dC = 2 * np.diag(np.sum(H * (J * R[:,m].reshape(K,1) - np.outer(J[:,m], KD)), axis = 1))
     
     # The covariance term
     grad_pld[m] += 0.5 * np.dot(A.T, np.dot(dS2dC, A))
-  
-    # The ln(det) term
+    
     if lndet:
-      grad_pld[m] -= np.trace(gp.solver.apply_inverse(dS2dC))
+      # The small ln(det) term
+      grad_pld[m] -= np.dot(KID, np.diag(dS2dC))
   
   grad_ll = np.append(gp.grad_lnlikelihood(fsum - pmod) / kernel.pars, grad_pld)
   
   return grad_ll  
   
-def AnalyticalGradient(coeffs, time, fsum, fpix, perr):
+def SlowAnalyticalGradient(coeffs, time, fsum, fpix, perr):
   
   # The pixel model
   pmod = np.sum(fpix * np.outer(1. / fsum, coeffs[2:]), axis = 1)
@@ -112,32 +131,6 @@ def AnalyticalGradient(coeffs, time, fsum, fpix, perr):
     B = gp.solver.apply_inverse(dSdC)
     
     grad_pld[m] = -0.5 * ( 2 * np.dot(dYdC.T, A) - np.dot(A.T, np.dot(dSdC, A)) + np.trace(B))
-  
-  grad_ll = np.append(gp.grad_lnlikelihood(fsum - pmod) / kernel.pars, grad_pld)
-  
-  return grad_ll
-
-def AnalyticalGradientIndependentCovariance(coeffs, time, fsum, fpix, perr):
-  
-  # The pixel model
-  pmod = np.sum(fpix * np.outer(1. / fsum, coeffs[2:]), axis = 1)
-
-  # Propagate errors correctly. ``T`` is the transit model, which is all 1's here since we're masking the transits
-  T = np.ones_like(fsum)
-  ferr = np.zeros_like(fsum)
-  for k, _ in enumerate(ferr):
-    ferr[k] = np.sqrt(np.sum(((1. / T[k]) + (pmod[k] / fsum[k]) - (coeffs[2:] / fsum[k])) ** 2 * perr[k] ** 2))
-
-  # Compute the likelihood
-  kernel = 1. * george.kernels.Matern32Kernel(1.)
-  kernel.pars = coeffs[:2]
-  gp = george.GP(kernel)
-  gp.compute(time, ferr)
-  ll = gp.lnlikelihood(fsum - pmod)
-
-  # Compute the gradient
-  A = -fpix.T / fsum
-  grad_pld = -np.dot(A, gp.solver.apply_inverse(fsum - pmod))
   
   grad_ll = np.append(gp.grad_lnlikelihood(fsum - pmod) / kernel.pars, grad_pld)
   
@@ -183,30 +176,26 @@ def NumericalGradient(coeffs, time, fsum, fpix, perr, eps = 1.e-6):
 
   return grad_ll
 
-koi = 17.01
-n = 0
-q = 4
-data = GetData(koi, data_type = 'bkg')
-quarter = data[q]
-time = quarter['time'][n]
-fpix = quarter['fpix'][n]
-fsum = quarter['fsum'][n]
-perr = quarter['perr'][n]
-npix = fpix.shape[1]
-coeffs = np.append(np.abs(np.random.randn(2) * 10), np.random.randn(npix) * fsum[0])
+if __name__ == '__main__':
+  '''
+  Run a timing test on quarter 4 of KOI 17.01.
+  
+  '''
+  
+  koi = 17.01
+  n = 2
+  q = 1
+  data = GetData(koi, data_type = 'bkg')
+  quarter = data[q]
+  time = quarter['time'][n]
+  fpix = quarter['fpix'][n]
+  fsum = quarter['fsum'][n]
+  perr = quarter['perr'][n]
+  npix = fpix.shape[1]
+  coeffs = np.append(np.abs(np.random.randn(2) * 10), np.random.randn(npix) * fsum[0])
 
+  n = lambda: NumericalGradient(coeffs, time, fsum, fpix, perr)
+  a = lambda: AnalyticalGradient(coeffs, time, fsum, fpix, perr)
 
-def ng():
-  return NumericalGradient(coeffs, time, fsum, fpix, perr, eps = 1e-6)
-
-def fg():
-  return FastAnalyticalGradient(coeffs, time, fsum, fpix, perr)
-
-def fg2():
-  return FastAnalyticalGradient(coeffs, time, fsum, fpix, perr, lndet = False)
-
-print(timeit.timeit(ng, number = 10))
-print(timeit.timeit(fg, number = 10))
-print(timeit.timeit(fg2, number = 10))
-
-
+  print('Numerical time: ', timeit.timeit(n, number = 10)/10)
+  print('Analytical time:', timeit.timeit(a, number = 10)/10)
