ece385919169455e07fec96870b5ae4bb0f49400
Rodrigo Luger <rodluger@gmail.com>
2015-10-23 14:22:05 -0700

    fix plotting issue

---
+++ scripts/plot.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+plot.py
+-------
+
+>>> python plot.py /path/to/input/script.py
+
+'''
+
+import matplotlib; matplotlib.use('Agg')
+import sys
+import os
+from eyepiece.detrend import PlotDetrended, PlotTransits
+
+if __name__ == '__main__':
+
+  # Grab input file from args
+  input_file = os.path.abspath(str(sys.argv[1]))
+  
+  # Plot
+  PlotDetrended(input_file = input_file, pool = pool)
+  PlotTransits(input_file = input_file, pool = pool)
