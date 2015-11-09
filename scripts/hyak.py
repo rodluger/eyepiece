6d808e75afab64902232c06e714b23eec87a9eb6
Rodrigo Luger <rodluger@gmail.com>
2015-11-09 08:28:13 -0800

    bugfix

--- scripts/hyak.py
+++ scripts/hyak.py
@@ -8,23 +8,30 @@ Runs ``eyepiece``. Call this script with ``mpi`` for
 MPI parallelization on Hyak. Note that the raw data must
 be cached prior to submitting the job. Simply run
 
->>> python -c "import eyepiece; eyepiece.Eyepiece('input.py')"
+>>> python -c "import eyepiece; eyepiece.Eyepiece()"
 
 before executing this script with ``mpi``:
 
->>> mpi hyak.py -i input.py
+>>> mpi hyak.py
 
 '''
 
 import matplotlib as mpl; mpl.use('Agg')
 from eyepiece import Eyepiece
 import para
+import sys
+
+# Did the user provide an input file?
+try:
+  inpfile = str(sys.argv[1])
+except:
+  inpfile = None
 
 # Create a ``para`` MPI pool instance
 pool = para.Pool(loadbalance = True)
 
 # Grab the data
-eye = Eyepiece('input.py')
+eye = Eyepiece(inpfile)
 
 # Detrend
 eye.Detrend(pool = pool)
