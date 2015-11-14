d948cdc004cb7c4786c22d0a393b1bb51bbb4279
Rodrigo Luger <rodluger@gmail.com>
2015-11-13 16:01:40 -0800

    debug

--- eyepiece/plot.py
+++ eyepiece/plot.py
@@ -340,6 +340,8 @@ def PlotTransits(input_file = None, ax = None, clobber = False):
       # Is this the correct way to whiten the transit flux?
       f = np.append(f, (ypld - mu + 1) * tmod)
 
+  import pdb; pdb.set_trace()
+
   # Plot
   if ax is None:
     userax = False
