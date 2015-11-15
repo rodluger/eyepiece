36037520f91847fe1a7e69463443cd5809beb933
Rodrigo Luger <rodluger@gmail.com>
2015-11-15 12:50:17 -0800

    more spaces

--- eyepiece/plot.py
+++ eyepiece/plot.py
@@ -171,7 +171,7 @@ def PlotDetrended(input_file = None, clobber = False):
       
       # Crowding
       crwd = prc[q]['crwd']
-      ax[0].annotate("CRWD: %.3f   " % crwd, (ltq, yb0), ha = 'right', va = 'bottom', fontsize = 8, color ='k')
+      ax[0].annotate("CRWD: %.3f     " % crwd, (ltq, yb0), ha = 'right', va = 'bottom', fontsize = 8, color ='k')
       
       if inp.plot_det_info:
         # Best coeff values
